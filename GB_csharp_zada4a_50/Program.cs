706f4abc688a506500da1794434a9dbe1f77830d
Rafil <ahref1@yandex.ru>
2022-08-17 19:54:57 +0300

    Кое-как выполнена ззадача  52

--- GB_csharp_zada4a_50/Program.cs
+++ GB_csharp_zada4a_50/Program.cs
@@ -7,73 +7,96 @@
 
 1 7 -> такого числа в массиве нет
 За основу принят код задачи 47
-Если это вещественные числа то навеоное можно с double не заморачиваться * условия задачи выполняются
- */
+nullable игнорируем
+поставленная задача выполняется, но очень много ошибок Nullable
+*/
 
+Console.Write($"Введите цифры первого и второго индекса массива\nчерез пробел: ");
+string strinput = Console.ReadLine();
 
+Console.Write($"Генерируем массив рандомный двухмерный массив:\n ");
+int[] ArrayIntIndex = new int[2];
+for (int i = 0; i < ArrayIntIndex.Length ;i++)
+{
+  ArrayIntIndex[i] = GenRandomInt();
+}
 int[,] ResultArray = new int[ArrayIntIndex[0], ArrayIntIndex[1]];
 
-int counter_baxpace = 0;
-for (int i = 0; i < ArrayIntIndex[1]; i++)
-{
-  counter_baxpace++;
-  if (counter_baxpace == ArrayIntIndex[1] || counter_baxpace == 1)
-  {
-    Console.Write("\t");
-  }
-  else
-  {
-    Console.Write("\n\t");
-  }
-  for (int j = 0; j < ArrayIntIndex[0]; j++)
-  {
-    ResultArray[j, i] = GenRandomInt();
-    Console.Write($" {ResultArray[j, i]} ");
-  }
-  if (counter_baxpace == ArrayIntIndex[1] - 1)
-  {
-    Console.Write("\n");
-  }
-  else
-  {
-    Console.Write("\t");
-  }
-}
-// вывод выглядит не очень, просчитывать длину символов в строке как-то не очень хочется))
-// минус функция красивого вывода
 
-static int GenRandomInt(int minval = 0, int maxval = 999)
+static int GenRandomInt(int minval = 1, int maxval = 999)
 {
   Random random = new();
   int result = random.Next(minval, maxval);
-  if (GenRandomSign() == 0)
-  {
-    return result;
-  }
-  else
-  {
-    return result * -1;
-  }
-}
-
-static int GenRandomSign()
-{
-  Random random = new();
-  int result = random.Next(0, 2);
   return result;
 }
 
-static int ParseIntInput()
+static List<string>? ParseStrInput(string inputstring)
 {
-  while (true)
+  try
   {
-    if (Int32.TryParse(Console.ReadLine(), out int result) == true && result > 0)
+    return inputstring.Split().ToList();
+  }
+  catch(Exception ex)
+  {
+    Console.WriteLine(ex.Message);
+    return null;
+  }
+}
+void generatearray()
+{
+  int counter_baxpace = 0;
+  for (int i = 0; i < ArrayIntIndex[1]; i++)
+  {
+    counter_baxpace++;
+    if (counter_baxpace == ArrayIntIndex[1] || counter_baxpace == 1)
     {
-      return result;
+      Console.Write("\t");
     }
     else
     {
-      Console.WriteLine($"Не INT, введите ещё раз: ");
+      Console.Write("\n\t");
+    }
+    for (int j = 0; j < ArrayIntIndex[0]; j++)
+    {
+      ResultArray[j, i] = GenRandomInt();
+      Console.Write($" {ResultArray[j, i]} ");
+    }
+    if (counter_baxpace == ArrayIntIndex[1] - 1)
+    {
+      Console.Write("\n");
+    }
+    else
+    {
+      Console.Write("\t");
     }
   }
+}
+
+static int ParseIntInput(string pstring)
+{
+  Int32.TryParse(pstring, out int result);
+  return result;
+}
+
+if (ParseStrInput(strinput) != null)
+{
+  generatearray(); 
+  if(  ParseIntInput(ParseStrInput(strinput)[0]) >= 0 
+    && ParseIntInput(ParseStrInput(strinput)[0]) <= ArrayIntIndex[0]
+    && ParseIntInput(ParseStrInput(strinput)[1]) >= 0
+    && ParseIntInput(ParseStrInput(strinput)[1]) <= ArrayIntIndex[1])
+  {
+
+    Console.WriteLine($"\nЗначение массива ResultArray с индексами: [{ArrayIntIndex[0]},{ArrayIntIndex[1]}]\n -> " +
+      $"с запрошенными индексами: {ResultArray[ParseIntInput(ParseStrInput(strinput)[0]), ParseIntInput(ParseStrInput(strinput)[1])]}.");
+  }
+  else
+  {
+    Console.WriteLine($"Результат для двухмерного массива ResultArray[{ArrayIntIndex[0]}," +
+      $"{ArrayIntIndex[1]}]\nОтсутствует.");
+  }
+}
+else
+{
+  Console.WriteLine($"Ввод данных неверный, запустите программу ещё раз.");
 }
