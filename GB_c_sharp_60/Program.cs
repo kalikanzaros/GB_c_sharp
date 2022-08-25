3f749c79f3599e086483065ef94db5b7870a871a
Rafil <ahref1@yandex.ru>
2022-08-25 19:02:46 +0300

    Задача 60 выполнена

---
+++ GB_c_sharp_60/Program.cs
@@ -0,0 +1,111 @@
+﻿/*
+
+
+ */
+
+
+zada4a58();
+void zada4a58()
+{
+  Console.Write($"Задача 58.Введите две цифры через пробел: ");
+  List<string> ListInputString = new List<string>();
+  int[] IndexFromList = new int[2];
+  ParseStrInput(Console.ReadLine());
+  int[,] ArrayOne = new int[IndexFromList[0], IndexFromList[1]];
+  int[,] ArrayTwo = new int[IndexFromList[0], IndexFromList[1]];
+  int[,] ArrayResult = new int[IndexFromList[0], IndexFromList[1]];
+  GenArray();
+  Console.Write($"Первый массив:\n");
+  PrintArray(ArrayOne);
+  Console.Write($"Второй массив:\n");
+  PrintArray(ArrayTwo);
+  Console.Write($"Результат умножения:\n");
+  PrintArray(ArrayResult);
+
+
+
+  void ParseStrInput(string inputstring)
+  {
+    bool whilecounter = false;
+
+    while (whilecounter == false)
+    {
+      if (inputstring.Split().ToList().Count() == 2 && !inputstring.Contains("-") && inputstring.Length >= 3
+          && (inputstring.Split()[0] != inputstring.Split()[1]))
+      {
+        ListInputString.AddRange(inputstring.Split().ToList());
+        whilecounter = true;
+      }
+      else
+      {
+        Console.Write($"Не похоже на два числа через пробел или цифры одинаковые, попробуем ещё раз: ");
+        ListInputString.Clear();
+        whilecounter = false;
+        inputstring = null;
+        ParseStrInput(Console.ReadLine());
+        break;
+      }
+    }
+    for (int i = 0; i < 2; i++)
+    {
+      if (ParseIntInput(ListInputString[i]) > 0)
+      {
+        IndexFromList[i] = ParseIntInput(ListInputString[i]);
+      }
+      else
+      {
+        Console.WriteLine($"Индексы меньше 1 не допускаются\nвведём индексы ещё раз:");
+        ListInputString.Clear();
+        inputstring = null;
+        ParseStrInput(Console.ReadLine());
+        break;
+      }
+    }
+  }
+
+  static int ParseIntInput(string pstring)
+  {
+    try
+    {
+      Int32.TryParse(pstring, out int result);
+      return result;
+    }
+    catch (Exception e)
+    {
+      Console.WriteLine($"ParseIntInput Error: {e.Message}");
+      return -1;
+    }
+  }
+
+  static int GenRandomDigit(int minndex = 2, int maxindex = 10)
+  {
+    Random random = new();
+    int result = random.Next(minndex, maxindex);
+    return result;
+  }
+
+  void GenArray()
+  {
+    for (int i = 0; i < IndexFromList[1]; i++)
+    {
+      for (int j = 0; j < IndexFromList[0]; j++)
+      {
+        ArrayOne[j, i] = GenRandomDigit();
+        ArrayTwo[j, i] = GenRandomDigit();
+        ArrayResult[j, i] = ArrayOne[j, i] * ArrayTwo[j, i];
+      }
+    }
+  }
+
+  void PrintArray(int[,] ints)
+  {
+    for (int i = 0; i < IndexFromList[1]; i++)
+    {
+      for (int j = 0; j < IndexFromList[0]; j++)
+      {
+        Console.Write($"{ints[j, i]}\t");
+      }
+      Console.Write($"\t\n");
+    }
+  }
+}
---
+++ GB_c_sharp_zada4a_60/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+массив размером 2 x 2 x 2
+12(0,0,0) 22(0,0,1)
+45(1,0,0) 53(1,0,1)
+ */
+
+
+zada4a60();
+void zada4a60()
+{
+  Console.Write($"Задача 60.Формируем трёхмерный массив из неповторяющихся двузначных чисел:\n");
+  int[,,] TreeArraay = new int[2, 2, 2];
+  int digitplus = 0;
+  GenArray();
+  
+  void GenArray()
+  {
+    for (int a = 0; a < 2; a++)
+    {
+      for (int b = 0; b < 2; b++)
+      {
+        for (int c = 0; c < 2; c++)
+        {
+          TreeArraay[a, b, c] = GenInt();
+          Console.Write($"{TreeArraay[a,b,c]}({a},{b},{c})\t");
+        }
+      }
+      Console.WriteLine();
+    }
+  }
+  int GenInt()
+  {
+    digitplus = digitplus + 1;
+    return digitplus;
+  }
+}
